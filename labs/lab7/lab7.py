31b1a29fe656d99fde588bab1f5c10c21e26ac05
ercochran <bob.cochran@hotmail.com>
2016-03-29 20:17:00 +0000

    lab work

--- labs/lab7/lab7.py
+++ labs/lab7/lab7.py
@@ -13,22 +13,22 @@
 
 # --------------------------------------------------------------------
 # Problem 1
-# 
+#
 # Fix ducklings' names
-# 
+#
 # In Robert McCloskey’s book Make Way for Ducklings, the names of the
 # ducklings are Jack, Kack, Lack, Mack, Nack, Ouack, Pack, and
 # Quack. This loop tries to output these names in order.
-# 
+#
 # Of course, that’s not quite right because Ouack and Quack are
 # misspelled. Can you fix it?
 #
-
+import lab7_solution as sol
 
 
 # --------------------------------------------------------------------
 # Problem 2
-# 
+#
 # Letter count
 #
 # Modify the count_letters function below so that:
@@ -39,24 +39,38 @@
 # Essentially, the function is returning the number of occurances of the
 # parameter char in the parameter string.
 
-
+def test_count_letters()
+    assert sol.count_letters("apple" , "a") == 2
+    assert sol.count_letters("bill" , "l") => 3
 
 # --------------------------------------------------------------------
 # Problem 3
 #
 # Reversing a string
-# 
+#
 # Complete the following function such that it reverses the parameter
 # string.
+
+def test_reverse_string()
+    assert sol.reverse_string("bill" , "llib") == True
+    assert sol.reverse_string("will" , "hubb") == False
+
 
 
 
 # --------------------------------------------------------------------
 # Problem 4
-# 
+#
 # Checking for palindromes
-# 
+#
 # Complete the following such that it correctly determines whether the
 # given parameter, string, is a palindrome
-# 
+#
+
+
+def test_is_palindrome()
+    assert sol.is_palindrome("bob") == True
+    assert sol.is_palindrome("bill") == False
+
+
 
